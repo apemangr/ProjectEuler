4208f28d5a0f07b511e860f29ed5b357e3be39ad
Guillermo Rojas <52636478+apemangr@users.noreply.github.com>
2022-05-29 16:31:01 -0400

    Problem 9

--- 97/main.cpp
+++ 97/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <boost/multiprecision/cpp_int.hpp>
+#include <string>
+#include <algorithm>
 
 using namespace std;
 
@@ -7,6 +9,16 @@ using namespace std;
 int main()
 {
   namespace mp = boost::multiprecision;
-  mp::cpp_int test =(28433*mp::pow(mp::cpp_int(2), 7830457)+1)/(10*2357197);
-  cout<<test;
+  string preResult;
+  mp::cpp_int temp;
+  mp::cpp_int test =(28433*mp::pow(mp::cpp_int(2), 7830457)+1);
+  for(int i = 1; i <= 10; i++)
+    {
+
+      temp = test % 10;
+      preResult.append(to_string(temp));
+      test /= 10;
+    }
+  reverse(preResult.begin(), preResult.end());
+  cout << preResult;
 }
