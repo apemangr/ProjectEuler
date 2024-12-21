86fbebc9b73b13513bf79bd5f3f914dc00bd45dd
Guillermo Rojas <apemangr@gmail.com>
2024-12-21 14:20:05 -0300

    63 added

--- 63/main.cpp
+++ 63/main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
-#include <wx>
+#include <cmath>
+
+using namespace std;
 
 int main()
 {
-    std::cout<<"Hola";
+    int c = 0;
+    for (int i = 1; i <= 9; i++)
+        for(int j = 1; j <= 22; j++)
+            if(floor(1+j * log10(i)) == j)
+                c++;
+
+    cout << "Answer: " << c << endl;
 }
